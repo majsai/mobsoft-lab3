37cf65414d2825747e7633f06aa9120d43013987
Majsai Ádám <majsaiadam@gmail.com>
2017-03-24 09:49:56 +0100

    MVP start

---
+++ app/src/main/java/com/mobsoft/mobilsoft/iotapp/ui/main/MainScreen.java
@@ -0,0 +1,11 @@
+package com.mobsoft.mobilsoft.iotapp.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mobsoft on 2017. 03. 24..
+ */
+
+public interface MainScreen {
+    void showMessage(String text);
+}
